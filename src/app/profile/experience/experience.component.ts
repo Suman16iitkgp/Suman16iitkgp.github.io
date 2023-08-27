06442fd8caaedb503077d3a6d1cd53d34c48d23b
Suman Kumar <48875251+Suman16iitkgp@users.noreply.github.com>
2023-08-27 22:08:14 +0530

    Removing Unused files

--- src/app/profile/experience/experience.component.ts
+++ src/app/profile/experience/experience.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileService } from '../profile.service';
-import { SplitPipe } from './split.pipe';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [CommonModule, SplitPipe],
+  imports: [CommonModule],
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss']
 })
